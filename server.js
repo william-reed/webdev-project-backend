cf8786318df40b0182ef71855e3c2c0845ce1cd8
William Reed <wreed58@gmail.com>
2018-06-16 17:55:02 -0400

    removed old cors

--- server.js
+++ server.js
@@ -12,16 +12,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // CORS
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin",
-        "http://localhost:4200");
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin",
+//         "http://localhost:4200");
+//     res.header("Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods",
+//         "GET, POST, PUT, DELETE, OPTIONS");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     next();
+// });
 
 // test mongo
 var db = mongoose.connection;
