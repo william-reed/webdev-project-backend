24a0dbc8d2f5b8e27e4dd1e0db9b800a779ba123
William Reed <wreed58@gmail.com>
2018-06-16 14:27:38 -0400

    added some more recurring services

--- services/recurring.service.server.js
+++ services/recurring.service.server.js
@@ -23,7 +23,47 @@ module.exports = function (app) {
             description: 'Receive random cat facts',
             fetch: () => axios.get('https://catfact.ninja/fact')
                 .then(response => response.data.fact)
+        },
+        {
+            title: 'Ron Swanson Quotes',
+            description: 'Get quotes from Park and Recreations infamous Ron Swanson (Nick Offerman) sent to your phone',
+            fetch: () => axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+                .then(response => response.data[0])
+        },
+        {
+            title: 'Dad Jokes',
+            description: 'Get some eye-rolling Dad inspired jokes.',
+            fetch: () => axios.get('https://icanhazdadjoke.com/', {
+                headers: {
+                    'accept': 'application/json',
+                }
+            }).then(response => response.data.joke)
+        },
+        {
+            title: 'Random Trivia',
+            description: 'Get random trivia sent to your phone',
+            fetch: () => axios.get('http://numbersapi.com/random/trivia')
+                .then(response => response.data)
+        },
+        {
+            title: 'Year Trivia',
+            description: 'Get random trivia about a random year',
+            fetch: () => axios.get('http://numbersapi.com/random/year')
+                .then(response => response.data)
+        },
+        {
+            title: 'Date Trivia',
+            description: 'Get trivia about a random date in history',
+            fetch: () => axios.get('http://numbersapi.com/random/date')
+                .then(response => response.data)
+        },
+        {
+            title: 'Math Trivia',
+            description: 'Get random math based facts',
+            fetch: () => axios.get('http://numbersapi.com/random/math')
+                .then(response => response.data)
         }
+
     ];
 
     function getRecurringReminders(req, res) {
